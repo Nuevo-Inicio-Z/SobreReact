f8baca1e107cc87d707fe23dd912e7cfb6f2566d
Zetien <jzettien@hotmail.com>
2021-12-01 11:52:20 -0500

    Agregada pagina a instalacion de react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,159 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/Styles.css';
+import logozetien from './media/zetien gamer logo beta.png'
+import fpsimagen from './media/fps.jpg'
+import carreraimagen from './media/racing.jpg'
+import aventuraimagen from './media/aventuras.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header>
+       <ul className="navbar">
+           <li>
+            <img src={logozetien} alt="logo" className="logo"/>
+           </li>
+           <li>
+            <button className="button mainbutton">Nuevo Post</button> 
+           </li>
+           <li>
+            <div className="buscar">
+             <input placeholder="Buscar un Juego" />
+             <i className="fas fa-search button iconoBusqueda"></i>
+            </div>
+           </li>
+            <li>
+            <button className="button secondarybutton">login</button>
+           </li>
+           <li>
+            <button className="button mainbutton">Registro</button>  
+           </li>
+            
+        </ul> 
+    </header>
+    <main>
+        <section>
+            <span className="mainTitle">
+              <h1>Tipos De Videojuegos</h1>
+            </span>
+            
+            <ul class="breedCardContainer">
+                <li className="breedCard">
+                    <div className="contenedorImagen">
+                         <img src={fpsimagen} alt="FPS"/>
+                    </div>
+                    <span className="breedTitle">
+                        Shooter
+                    </span>
+                    <div>
+                        <ul className="movWeb">
+                            <li>
+                                <div>
+                                    <button className="botonesWeb">
+                                        <span className="colorLikes"><i class="fas fa-heart"></i></span>
+                                        <span className="textoInforme">150</span> 
+                                        
+                                    </button>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <button className="botonesWeb">
+                                        <span className="colorIconsCard"><i class="fas fa-comment"></i></span>
+                                        <span className="textoInforme">1.5K</span>
+                                    </button>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <button className="botonesWeb">
+                                        <span className="colorIconsCard"><i class="far fa-eye"></i></span>
+                                        <span className="textoInforme">120</span>
+                                    </button>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li className="breedCard">
+                    <div className="contenedorImagen">
+                        <img src={carreraimagen} alt="carreras"/>
+                    </div>
+                    <span className="breedTitle">
+                        Racing
+                    </span>
+                    <div>
+                        <ul className="movWeb">
+                            <li>
+                                <div>
+                                    <button className="botonesWeb">
+                                        <span className="colorLikes"><i class="fas fa-heart"></i></span>
+                                        <span className="textoInforme">450</span> 
+                                        
+                                    </button>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <button className="botonesWeb">
+                                        <span className="colorIconsCard"><i class="fas fa-comment"></i></span>
+                                        <span className="textoInforme">1.1K</span>
+                                    </button>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <button className="botonesWeb">
+                                        <span className="colorIconsCard"><i class="far fa-eye"></i></span>
+                                        <span className="textoInforme">850</span>
+                                    </button>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li className="breedCard">
+                    <div className="contenedorImagen">
+                        <img src={aventuraimagen} alt="Aventuras"/>
+                    </div>
+                    <span className="breedTitle">
+                        Aventuras
+                    </span>
+                    <div>
+                        <ul className="movWeb">
+                            <li>
+                                <div>
+                                    <button className="botonesWeb">
+                                        <span className="colorLikes"><i class="fas fa-heart"></i></span>
+                                        <span className="textoInforme">300</span> 
+                                        
+                                    </button>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <button className="botonesWeb">
+                                        <span className="colorIconsCard"><i class="fas fa-comment"></i></span>
+                                        <span className="textoInforme">1.5K</span>
+                                    </button>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <button className="botonesWeb">
+                                        <span className="colorIconsCard"><i class="far fa-eye"></i></span>
+                                        <span className="textoInforme">300</span>
+                                    </button>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </section>
+        <section></section>
+    </main>
+    <footer></footer>
     </div>
   );
 }
